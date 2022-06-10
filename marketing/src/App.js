7928e210037657e7ae4880fb2e731e7976cb7945
Ryan Rhodes <ryan@ryanrhodes.io>
2022-06-10 12:37:50 -0400

    fix css conflict bug

--- marketing/src/App.js
+++ marketing/src/App.js
@@ -1,23 +1,27 @@
 import React from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
+import {StylesProvider, createGenerateClassName} from '@material-ui/core/styles';
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma'
+})
+
 const App = () => {
-    return (
-        <div>
-            <StylesProvider>
-                <BrowserRouter>
-                    <Switch>
-                        <Route exact path="/pricing" component={Pricing} />
-                        <Route exact path="/" component={Landing} />
-                    </Switch>
-                </BrowserRouter>
-            </StylesProvider>
-        </div>
-    )
+  return (
+    <div>
+      <StylesProvider generateClassName={generateClassName}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/pricing" component={Pricing}/>
+            <Route exact path="/" component={Landing}/>
+          </Switch>
+        </BrowserRouter>
+      </StylesProvider>
+    </div>
+  )
 }
 
 export default App;
